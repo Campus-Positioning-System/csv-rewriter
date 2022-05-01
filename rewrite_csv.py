5f22754b904f3771f8998d2e9dc2c44684376a86
fischerjulius <66005843+fischerjulius@users.noreply.github.com>
2022-05-01 12:02:06 +0200

    Update rewrite_csv.py

--- rewrite_csv.py
+++ rewrite_csv.py
@@ -5,11 +5,9 @@ with open("AP-Referenzdaten.csv", 'r') as readfile:
     reader = csv.reader(readfile, delimiter=';')
     header = next(reader)
     for row in reader:
-        count_read += 1
         ssid = row[1]
         if "HFU" in ssid or "eduroam" in ssid or "LAB" in ssid or "Lab" in ssid:
             matches.append(row)
-            count_write += 1
 with open("AP-Referenzdaten_filtered.csv", 'w', newline='') as writefile:
     writer = csv.writer(writefile, delimiter=';')
     writer.writerow(header)

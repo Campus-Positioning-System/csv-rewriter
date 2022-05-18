cc0a02aaae5ad6b475aeef41dd69aac182e6bf11
fischerjulius <juliusfischer.fds@gmail.com>
2022-05-18 07:47:37 +0200

    A,B,C-Bau + "Grad"-Spalte

--- rewrite_csv.py
+++ rewrite_csv.py
@@ -1,14 +1,14 @@
 import csv
 
 matches = []
-with open("AP-Referenzdaten.csv", 'r') as readfile:
+with open("AP_ref_combined.csv", 'r') as readfile:
     reader = csv.reader(readfile, delimiter=';')
     header = next(reader)
     for row in reader:
-        ssid = row[1]
+        ssid = row[2]
         if "HFU" in ssid or "eduroam" in ssid or "LAB" in ssid or "Lab" in ssid:
             matches.append(row)
-with open("AP-Referenzdaten_filtered.csv", 'w', newline='') as writefile:
+with open("AP_reference_filtered.csv", 'w', newline='') as writefile:
     writer = csv.writer(writefile, delimiter=';')
     writer.writerow(header)
     for row in matches:

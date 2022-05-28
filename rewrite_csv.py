e92bb11b6dc3b752501ea909c5bd06e04dea077c
fischerjulius <juliusfischer.fds@gmail.com>
2022-05-28 10:39:38 +0200

    Punkte->Koordinaten

--- rewrite_csv.py
+++ rewrite_csv.py
@@ -1,14 +1,26 @@
 import csv
 
+filtered = []
+coords = []
 matches = []
-with open("AP_ref_combined.csv", 'r') as readfile:
-    reader = csv.reader(readfile, delimiter=';')
-    header = next(reader)
-    for row in reader:
-        ssid = row[2]
+with open("csv_combined_unfiltered.csv", 'r') as readCSV:
+    readerCSV = csv.reader(readCSV, delimiter=';')
+    header = next(readerCSV)
+    for rowCSV in readerCSV:
+        ssid = rowCSV[2]
         if "HFU" in ssid or "eduroam" in ssid or "LAB" in ssid or "Lab" in ssid:
-            matches.append(row)
-with open("AP_reference_filtered.csv", 'w', newline='') as writefile:
+            filtered.append(rowCSV)
+with open("nodereference.csv", 'r') as readRef:
+    readerRef = csv.reader(readRef, delimiter=';')
+    headerRef = next(readerRef)
+    for rowRef in readerRef:
+        coords.append(rowRef)
+for k in range(len(coords)):
+    for l in range(len(filtered)):
+        if coords[k][0] == filtered[l][0]:
+            filtered[l][0] = coords[k][1]
+            matches.append(filtered[l])
+with open("csv_combined_filtered.csv", 'w', newline='') as writefile:
     writer = csv.writer(writefile, delimiter=';')
     writer.writerow(header)
     for row in matches:
